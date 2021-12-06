dede4dba64cd1dccf065d8777cc9a8501c57ef52
Danielvalev <danielvalev89@gmail.com>
2021-12-06 12:03:16 +0200

    [added] description in restaurant model

--- kutiika/restaurants/models.py
+++ kutiika/restaurants/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField('Restaurant Name', max_length=120)
     address = models.CharField(max_length=300)
+    description = models.CharField('Description', max_length=255)
     phone = models.CharField('Contact Phone', max_length=25, blank=True)
     web = models.URLField('Website Address', blank=True)
     logo = models.ImageField(upload_to='public/restaurant_logos', blank=True, null=True)
