c3e94a9f69d18cd0d2b5194f2b332e024e75713f
Danielvalev <danielvalev89@gmail.com>
2021-12-07 21:06:51 +0200

    [added] login, logout users and messages

--- kutiika/customers/urls.py
+++ kutiika/customers/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
+    path('login/', views.login_customer, name='login'),
+    path('logout/', views.logout_customer, name='logout'),
+    path('register/', views.register_customer, name='register'),
 ]
--- kutiika/customers/views.py
+++ kutiika/customers/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 # Create your views here.
-def login(request):
-    return render(request, 'customers/login.html')
+def login_customer(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You are logged in')
+            return redirect('home')
+        else:
+            messages.success(request, 'The username or password is incorrect')
+            return redirect('login')
+    else:
+        return render(request, 'customers/login.html', {
+
+        })
 
 
-def register(request):
+def logout_customer(request):
+    logout(request)
+    messages.success(request, 'You Were Logged Out')
+    return redirect('home')
+
+
+def register_customer(request):
     return render(request, 'customers/register.html')
