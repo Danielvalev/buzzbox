3717e01ed7c848789e1924ce66f1facd9c92a416
Danielvalev <danielvalev89@gmail.com>
2021-12-08 19:39:04 +0200

    [added] Dish admin filters

--- kutiika/restaurants/admin.py
+++ kutiika/restaurants/admin.py
@@ -4,6 +4,16 @@ from .models import Restaurant, FoodCategory, Dish
 # Register your models here.
 admin.site.register(Restaurant)
 admin.site.register(FoodCategory)
-admin.site.register(Dish)
 
-# TODO add more filters to Dish and Restaurant
+
+# admin.site.register(Dish)
+
+# TODO add more filter to Restaurant
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    fields = ('name', 'portion_size', 'price', 'restaurant_dish', 'food_category')
+    list_display = ('name', 'food_category', 'restaurant_dish', 'price')
+    list_filter = ('restaurant_dish', 'food_category')
+    ordering = ('restaurant_dish',)
+
+
