bd7836fcdac5d90a6e0d29e9064c8378deddc331
Danielvalev <danielvalev89@gmail.com>
2021-12-08 17:00:05 +0200

    [added] Dish and FoodCategory models

--- kutiika/restaurants/admin.py
+++ kutiika/restaurants/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Restaurant
+from .models import Restaurant, FoodCategory, Dish
 
 # Register your models here.
 admin.site.register(Restaurant)
+admin.site.register(FoodCategory)
+admin.site.register(Dish)
+
+# TODO add more filters to Dish and Restaurant
--- kutiika/restaurants/models.py
+++ kutiika/restaurants/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import PROTECT
 
 
 # Create your models here.
@@ -14,3 +15,27 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return f'{self.name} | Manager: {self.manager}'
+
+
+class FoodCategory(models.Model):
+    name = models.CharField('Food Category', max_length=50)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Dish(models.Model):
+    name = models.CharField('Dish Name', max_length=100)
+    portion_size = models.IntegerField('Portion Size')
+    price = models.DecimalField('Price', max_digits=5, decimal_places=2)
+    restaurant_dish = models.ForeignKey(Restaurant, blank=True, null=True, on_delete=models.CASCADE)
+    food_category = models.ForeignKey(FoodCategory, on_delete=PROTECT)
+
+    def __str__(self):
+        return f'{self.food_category} | {self.name} | {self.portion_size} gr. | {self.price} BGN'
+
+
+class Menu(models.Model):
+    # TODO - Must be linked to Restaurant and to Dish
+    # TODO - Must have date
+    pass
