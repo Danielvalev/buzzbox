104c9787386b0445694371ac548e433e73a3d9a2
Danielvalev <danielvalev89@gmail.com>
2021-12-09 17:04:54 +0200

    [added] daily menu filter and menu csv export

--- kutiika/restaurants/urls.py
+++ kutiika/restaurants/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('dashboard/', views.restaurant_dashboard, name='restaurant-dashboard'),
     path('', views.restaurants_list, name='restaurants-list'),
-    path('menu/', views.restaurants_menu, name='restaurant-menu'),
+    path('menu/<restaurant_id>/', views.restaurants_menu, name='restaurant-menu'),
+    path('menu_csv/<restaurant_id>/', views.menu_csv, name='menu-csv'),
 ]
--- kutiika/restaurants/views.py
+++ kutiika/restaurants/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Restaurant, Menu, Dish
+from datetime import date
+import csv
+from django.http import HttpResponseRedirect, HttpResponse, FileResponse
 
 
 # Create your views here.
@@ -14,8 +17,13 @@ def restaurants_list(request):
     })
 
 
-def restaurants_menu(request):
-    menus = Menu.objects.all()
+def restaurants_menu(request, restaurant_id=1):
+    today = str(date.today())
+    year, month, day = today.split('-')
+
+    # menus = Menu.objects.filter(restaurant_menu=restaurant_id).order_by('-menu_date')[:1]  # Show latest menu by date
+    menus = Menu.objects.filter(restaurant_menu=restaurant_id, menu_date__year=year, menu_date__month=month,
+                                menu_date__day=day)  # Show today menu
 
     # TODO Create a better sorting function
     # Sorting Dishes
@@ -47,3 +55,32 @@ def restaurants_menu(request):
         'desserts': desserts,
         'other_dishes': other_dishes,
     })
+
+
+# Generate CSV File Menu
+def menu_csv(request, restaurant_id):
+    # Designate The Model
+    today = str(date.today())
+    year, month, day = today.split('-')
+
+    filename = f'menu_{day}_{month}_{year}'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename={filename}.csv'
+
+    # Create a csv writer
+    writer = csv.writer(response)
+
+    # menus = Menu.objects.filter(restaurant_menu=restaurant_id).order_by('-menu_date')[:1]  # Show latest menu by date
+    menus = Menu.objects.filter(restaurant_menu=restaurant_id, menu_date__year=year, menu_date__month=month,
+                                menu_date__day=day)  # Show today menu
+
+    # Add column headings to the csv file
+    writer.writerow(['Menu Date', 'Dish Name', 'Dish Category', 'Restaurant', 'Portion Size', 'Price'])
+
+    # Loop Thu and output
+    for menu in menus:
+        for dish in menu.dish_menu.all():
+            writer.writerow([menu.menu_date, dish.name, dish.food_category, dish.restaurant_dish,
+                             dish.portion_size, dish.price])
+
+    return response
